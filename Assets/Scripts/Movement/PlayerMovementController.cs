ab00ea334db43fea93e79ba92c5acee47f6a99ef
Kiddalingur <58945628+Kiddalingur@users.noreply.github.com>
2022-07-04 15:23:34 +0200

    Working movement and camera in multiplayer

--- Assets/Scripts/Movement/PlayerMovementController.cs
+++ Assets/Scripts/Movement/PlayerMovementController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Mirror;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerMovementController : NetworkBehaviour
 {
@@ -24,16 +25,27 @@ public class PlayerMovementController : NetworkBehaviour
     const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
     const string yAxis = "Mouse Y";
 
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera Camera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
 
-    public CharacterController controller;
-    
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
 
 
     public float Speed = 0.1f;
     public GameObject PlayerModel;
 
     public float mouseSensitivity = 100f;
-    public GameObject Camera;
+    //public GameObject Camera;
 
     public Transform playerBody;
 
@@ -43,6 +55,13 @@ public class PlayerMovementController : NetworkBehaviour
     {
         //Cursor.lockState = CursorLockMode.Locked;
         PlayerModel.SetActive(false);
+        characterController = GetComponent<CharacterController>();
+
+        if (!isLocalPlayer)
+        {
+            Camera.gameObject.SetActive(false);
+        }
+
 
     }
 
@@ -79,12 +98,40 @@ public class PlayerMovementController : NetworkBehaviour
     public void Movement()
     {
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        {
+            // We are grounded, so recalculate move direction based on axes
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            // Press Left Shift to run
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                moveDirection.y = movementDirectionY;
+            }
 
-        controller.Move(move * Speed * Time.deltaTime);
+            if (!characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            characterController.Move(moveDirection * Time.deltaTime);
+            if (canMove)
+            {
+                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+                Camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            }
+        }
 
 
 
